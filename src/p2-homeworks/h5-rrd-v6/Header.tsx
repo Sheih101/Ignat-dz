e67557dd1f99ad8c0275321a8ae3a675f29ef457
Sheih <sheihdog325@gmail.com>
2022-05-04 18:58:46 +0300

    activeClass fix

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -6,9 +6,9 @@ import s from './Header.module.css'
 function Header() {
     return (
         <div className={s.header}>
-            <NavLink to={PATH.PRE_JUNIOR} className={s.link}>pre-junior</NavLink>
-            <NavLink to={PATH.JUNIOR} className={s.link}>junior</NavLink>
-            <NavLink to={PATH.JUNIOR_PLUS} className={s.link}>junior+</NavLink>
+            <NavLink to={PATH.PRE_JUNIOR} className={({isActive}) => isActive ? s.active : s.link}>pre-junior</NavLink>
+            <NavLink to={PATH.JUNIOR} className={({isActive}) => isActive ? s.active : s.link}>junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS} className={({isActive}) => isActive ? s.active : s.link}>junior+</NavLink>
             <div className={s.block}/>
         </div>
     )
